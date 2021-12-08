1f8ffe040f666a0b37fb43254ca76fdab9f64b0d
Matthieu <matthieu.petit.contact@gmail.com>
2021-12-08 01:56:23 +0100

    change days to mid-days and add harvest steps

--- public/main.js
+++ public/main.js
@@ -1,40 +1,63 @@
 // Hist APRS : 620, 618.5
 
-let init_invest = 1000;
-let for_days = 30;
-let init_apr = 619.67;
-let harvest_fees = 0.6;
-let apr_down = true;
+const inputs = {
+  init_invest: 1000,
+  for_days: 30,
+  init_apr: 619.67,
+  harvest_fees: 0.6,
+  apr_down: true,
+};
+loadFromStorage();
 
-document.querySelector('#init_invest').value = init_invest;
+document.querySelector('#init_invest').value = inputs.init_invest;
 document.querySelector('#init_invest').addEventListener('change', (e) => {
-  init_invest = parseInt(e.target.value);
+  inputs.init_invest = parseInt(e.target.value);
   updateChart();
 });
-document.querySelector('#for_days').value = for_days;
+document.querySelector('#for_days').value = inputs.for_days;
 document.querySelector('#for_days').addEventListener('change', (e) => {
-  for_days = parseInt(e.target.value);
+  inputs.for_days = parseInt(e.target.value);
   updateChart();
 });
-document.querySelector('#init_apr').value = init_apr;
+document.querySelector('#init_apr').value = inputs.init_apr;
 document.querySelector('#init_apr').addEventListener('change', (e) => {
-  init_apr = parseFloat(e.target.value);
+  inputs.init_apr = parseFloat(e.target.value);
   updateChart();
 });
-document.querySelector('#harvest_fees').value = harvest_fees;
+document.querySelector('#harvest_fees').value = inputs.harvest_fees;
 document.querySelector('#harvest_fees').addEventListener('change', (e) => {
-  harvest_fees = parseFloat(e.target.value);
+  inputs.harvest_fees = parseFloat(e.target.value);
   updateChart();
 });
-document.querySelector('#apr_down').checked = apr_down;
+document.querySelector('#apr_down').checked = inputs.apr_down;
 document.querySelector('#apr_down').addEventListener('change', (e) => {
-  apr_down = e.target.checked;
+  inputs.apr_down = e.target.checked;
   updateChart();
 });
 
 function updateChart() {
+  harvest_step = (inputs.init_invest * inputs.init_apr) / (365 * 2) / 100;
+  saveToStorage();
   chart.data = initChart();
   chart.update();
+}
+
+function saveToStorage() {
+  ['init_invest', 'for_days', 'init_apr', 'harvest_fees', 'apr_down'].forEach((item) => {
+    localStorage.setItem(item, inputs[item]);
+  });
+}
+
+function loadFromStorage() {
+  ['init_invest', 'for_days', 'init_apr', 'harvest_fees', 'apr_down'].forEach((item) => {
+    const value = localStorage.getItem(item);
+
+    if (value !== null) {
+      if (value.includes('.')) inputs[item] = parseFloat(value);
+      else if (['true', 'false'].includes(value)) inputs[item] = value === 'true';
+      else inputs[item] = parseInt(value);
+    }
+  });
 }
 
 function getRandomColor() {
@@ -47,38 +70,42 @@ function getRandomColor() {
 }
 
 function getNextAPR(current_apr) {
-  if (apr_down) {
+  if (inputs.apr_down) {
     return Math.max(25, current_apr * 0.97);
   } else {
     return current_apr;
   }
 }
 
-function compoundEvery(days) {
+// For each day : check 2x if rewards are interesting
+// to harvest, and add a line for this "step reward"
+let harvest_step = (inputs.init_invest * inputs.init_apr) / (365 * 2) / 100;
+
+function compoundEvery(harvest_mult) {
   const compound = [];
-  let current = init_invest;
-  let apr = init_apr;
+
+  let current = inputs.init_invest;
+  let apr = inputs.init_apr;
   let accu = 0;
   compound.push(current);
 
-  for (let day = 1; day < for_days; day++) {
-    const daily_apr = apr / 365;
-    const daily_gain = (daily_apr / 100) * current;
-    accu += daily_gain;
+  for (let day = 1; day < inputs.for_days * 2; day++) {
+    const mid_daily_apr = apr / (365 * 2);
+    const mid_daily_gain = (mid_daily_apr / 100) * current;
+    accu += mid_daily_gain;
 
-    if (day % days == 0) {
-      current += accu;
-      current -= harvest_fees;
+    if (accu >= harvest_step * harvest_mult) {
+      current += accu - inputs.harvest_fees;
       accu = 0;
     }
-
     compound.push(current);
+
     apr = getNextAPR(apr);
   }
 
   // Harvest for last day
-  if (accu != 0) {
-    compound[for_days - 1] += accu - harvest_fees;
+  if (accu !== 0) {
+    compound[compound.length - 1] += accu - inputs.harvest_fees;
   }
   return compound;
 }
@@ -88,7 +115,7 @@ function initChart() {
     labels: [],
     datasets: [
       {
-        label: `Harvest every day`,
+        label: `Harvest every ${harvest_step.toFixed(2) * 1}`,
         data: compoundEvery(1),
         borderColor: getRandomColor(),
         fill: false,
@@ -100,12 +127,13 @@ function initChart() {
 
   while (1) {
     const current_compound_every = data.datasets.length + 1;
-    const last_max = data.datasets[data.datasets.length - 1].data[for_days - 1];
+    const last_data = data.datasets[data.datasets.length - 1].data;
+    const last_max = last_data[last_data.length - 1];
     const current_compound = compoundEvery(current_compound_every);
 
-    if (current_compound[for_days - 1] > last_max + 2) {
+    if (current_compound[current_compound.length - 1] >= last_max) {
       data.datasets.push({
-        label: `Harvest every ${current_compound_every} days`,
+        label: `Harvest every ${harvest_step.toFixed(2) * current_compound_every}`,
         data: current_compound,
         borderColor: getRandomColor(),
         fill: false,
@@ -113,15 +141,28 @@ function initChart() {
         type: 'line',
       });
     } else {
+      // data.datasets.push({
+      //   label: `Harvest every ${harvest_step.toFixed(2) * current_compound_every}`,
+      //   data: current_compound,
+      //   borderColor: getRandomColor(),
+      //   fill: false,
+      //   stepped: false,
+      //   type: 'line',
+      // });
       break;
     }
+
+    // if (current_compound_every >= 10) {
+    //   break;
+    // }
   }
 
-  let prev_apr = init_apr;
+  // Set APR line
+  let prev_apr = inputs.init_apr;
   data.datasets.push({
     label: `APR (%)`,
-    data: Array.from(Array(for_days).keys()).map((_, i) => {
-      prev_apr = i === 0 ? init_apr : getNextAPR(prev_apr);
+    data: Array.from(Array(inputs.for_days * 2).keys()).map((_, i) => {
+      prev_apr = i === 0 ? inputs.init_apr : getNextAPR(prev_apr);
       return prev_apr;
     }),
     borderColor: 'red',
@@ -130,14 +171,21 @@ function initChart() {
     type: 'line',
   });
 
-  data.labels = Array.from(Array(for_days).keys()).map((day) => `Day ${day + 1}`);
+  // Set bottom labels
+  data.labels = Array.from(Array(inputs.for_days * 2).keys()).map(
+    (day) => `Day ${day / 2}`
+  );
 
-  const last_max = data.datasets[data.datasets.length - 2].data[for_days - 1].toFixed(0);
-  const result_percent = (((last_max - init_invest) / init_invest) * 100).toFixed(1);
-  let innerHTML = 'Harvest every XXX day(s) for YYY days : ZZZ';
+  const last_data = data.datasets[data.datasets.length - 2].data;
+  const last_max = last_data[last_data.length - 1].toFixed(0);
+  const result_percent = (
+    ((last_max - inputs.init_invest) / inputs.init_invest) *
+    100
+  ).toFixed(1);
+  let innerHTML = 'XXX$ for YYY days : ZZZ';
   document.querySelector('#results').innerHTML = innerHTML
-    .replace('XXX', data.datasets.length - 1)
-    .replace('YYY', for_days)
+    .replace('XXX', data.datasets[data.datasets.length - 2].label)
+    .replace('YYY', inputs.for_days)
     .replace('ZZZ', `${last_max} (+ ${result_percent}%)`);
 
   addToHist(data.datasets.length - 1, last_max, result_percent);
@@ -147,14 +195,13 @@ function initChart() {
 
 const table = document.querySelector('#hist');
 function addToHist(compound, last_max, result_percent) {
-  console.log(last_max);
   const row = table.insertRow(1);
-  row.insertCell(0).innerHTML = init_invest;
-  row.insertCell(1).innerHTML = init_apr;
-  row.insertCell(2).innerHTML = apr_down ? 'yes' : 'no';
-  row.insertCell(3).innerHTML = `${for_days} day${for_days > 1 ? 's' : ''}`;
+  row.insertCell(0).innerHTML = inputs.init_invest;
+  row.insertCell(1).innerHTML = inputs.init_apr;
+  row.insertCell(2).innerHTML = inputs.apr_down ? 'yes' : 'no';
+  row.insertCell(3).innerHTML = `${inputs.for_days} day${inputs.for_days > 1 ? 's' : ''}`;
   row.insertCell(4).innerHTML = `${compound} day${compound > 1 ? 's' : ''}`;
-  row.insertCell(5).innerHTML = harvest_fees;
+  row.insertCell(5).innerHTML = inputs.harvest_fees;
   row.insertCell(6).innerHTML = last_max;
   row.insertCell(7).innerHTML = `${result_percent}%`;
 }
